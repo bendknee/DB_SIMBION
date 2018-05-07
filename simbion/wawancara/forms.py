49b901f9d2ca3a52621ccbce6e0e253049aa9616
Benny William <Pardede(bpardedewilliam@gmail.com)>
2018-05-07 15:51:05 +0700

    Add new navbar Wawancara

--- simbion/wawancara/forms.py
+++ simbion/wawancara/forms.py
@@ -2,8 +2,11 @@ from django import forms
 
 
 class WawancaraForm(forms.Form):
+    error_messages = {'required': 'Tolong isi input ini.'}
     attrs = {
-        'class': 'form-control'
+        'class': 'form-textinput',
+        'cols': 50,
+        'rows': 4
     }
     kode = forms.IntegerField(label='Kode', required=True, widget=forms.NumberInput(attrs=attrs))
     nama = forms.CharField(label='Nama', required=True, max_length=50, widget=forms.TextInput(attrs=attrs))
