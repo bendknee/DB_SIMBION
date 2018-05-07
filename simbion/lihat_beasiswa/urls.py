3677a89d85eaf3ca94a5b424a7b2285dcfcb0910
Ahmad Hasan Siregar <a.hasan.gar@gmail.com>
2018-05-07 16:58:32 +0700

    Implement "Melihat Informasi Beasiswa Aktif" 2/2
    Show one mock Beasiswa data

--- simbion/lihat_beasiswa/urls.py
+++ simbion/lihat_beasiswa/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, detail_beasiswa
 
 app_name = 'lihat_beasiswa'
 
 urlpatterns = [
 	path('', index, name='index'),
+	path('detail/', detail_beasiswa, name='detail-beasiswa'),
 ]
--- simbion/lihat_beasiswa/views.py
+++ simbion/lihat_beasiswa/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
-mock_beasiswa1 = {"nama" : "Bank of Tokyo", "tanggal_tutup" : "31/05/2018", "status" : "Dibuka", "jumlah_pendaftar" : 50}
+mock_beasiswa1 = {"kode" : 82390, "nama" : "Bank of Tokyo", "jenis" : "Beasiswa Bantuan Belajar", "tanggal_tutup" : "31/05/2018",
+				"donatur" : "DON123" , "status" : "Dibuka", "jumlah_pendaftar" : 50,
+				"deskripsi" : " Besaran yang didapatkan dari beasiswa ini adalah sebesar Rp6.000.000,-/mahasiswa setiap semester untuk pembayaran biaya pendidikan.",
+				"syarat" : "IPK >= 3,25"}
 mock_beasiswa2 = {"nama" : "BEASISWA KADER SURAU YBM BRI TAHUN 2018", "tanggal_tutup" : "30/04/2018", "status" : "Ditutup", "jumlah_pendaftar" : 24}
+
 response = {}
 
 def index(request):
@@ -11,4 +15,9 @@ def index(request):
 	response["beasiswa_list"] = beasiswa_list
 
 	html = "lihat_beasiswa/lihat_beasiswa.html"
+	return render(request, html, response)
+
+def detail_beasiswa(request):
+	html = "lihat_beasiswa/detail_beasiswa.html"
+	response["beasiswa"] = mock_beasiswa1
 	return render(request, html, response)
